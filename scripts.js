5d1f251844b9776cb70c10f44667c4c624608214
David van den Bor <david@davidvandenbor.nl>
2020-12-28 20:39:56 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,13 @@
 
+// Let's make a custom made style property that we can use in the CSS file. It listenes to the scroll event. We'll call it "--scrollbar"!
+
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.style.setProperty(
+      "--scrollbar",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
+    );
+  },
+  false
+);
